0693f2a5a05e97cbc1a3bcd741d87e3cbf62630a
mhamza219 <mhamza219@gmail.com>
2023-05-12 14:52:51 +0530

    todo integrate

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,62 +1,111 @@
-import React from 'react'
+// import React from 'react'
+// import '../css/Gallery.css';
 
-import img1 from '../Img/img1.jpeg';
-import img2 from '../Img/img2.jpeg';
-import img3 from '../Img/img3.jpeg';
-import img4 from '../Img/img4.jpeg';
-import img5 from '../Img/img5.jpeg';
-import img6 from '../Img/img6.jpeg';
-import img7 from '../Img/img7.jpeg';
+// import img1 from '../Img/img1.jpeg';
+// import img2 from '../Img/img2.jpeg';
+// import img3 from '../Img/img3.jpeg';
+// import img4 from '../Img/img4.jpeg';
+// import img5 from '../Img/img5.jpeg';
+// import img6 from '../Img/img6.jpeg';
+// import img7 from '../Img/img7.jpeg';
 
 
-const  Gallery = () => {
+// const  Gallery = () => {
 
-	let data = [
-			{
-				id: 1,
-				imgSrc: img1,
-			},
-			{
-				id: 2,
-				imgSrc: img2,
-			},
-			{
-				id: 3,
-				imgSrc: img3,
-			},
-			{
-				id: 4,
-				imgSrc: img4,
-			},
-			{
-				id: 5,
-				imgSrc: img5,
-			},
-			{
-				id: 6,
-				imgSrc: img6,
-			},
-			{
-				id: 7,
-				imgSrc: img7,
-			},
+// 	let data = [
+// 			{
+// 				id: 1,
+// 				imgSrc: img1,
+// 			},
+// 			{
+// 				id: 2,
+// 				imgSrc: img2,
+// 			},
+// 			{
+// 				id: 3,
+// 				imgSrc: img3,
+// 			},
+// 			{
+// 				id: 4,
+// 				imgSrc: img4,
+// 			},
+// 			{
+// 				id: 5,
+// 				imgSrc: img5,
+// 			},
+// 			{
+// 				id: 6,
+// 				imgSrc: img6,
+// 			},
+// 			{
+// 				id: 7,
+// 				imgSrc: img7,
+// 			},
 
-		]
-	return(
-		<>
-			<div className="gallery">
-				{data.map((item, index)=>{
-					return(
-						<div className="pics" key={index}>
-							<img src={item.imgSrc}/>
+// 		]
+// 	return(
+// 		<>
+// 			<div className="gallery">
+// 				{data.map((item, index)=>{
+// 					return(
+// 						<div className="pics" key={index}>
+// 							<img src={item.imgSrc} onClick={handleOpen} style={{width: '20%'}}/>
 
-						</div>
-					)
-				})}
-			</div>
-		</>
+// 						</div>
+// 					)
+// 				})}
+// 			</div>
+// 		</>
 		
-	)
-}
+// 	)
+// }
 
-export default Gallery;
+// export default Gallery;
+
+
+
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BasicModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
---
+++ src/components/Todo.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={newTodo} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
