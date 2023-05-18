3c0273a4beca079bb1a0a0704305b7f2fcc6ef01
mhamza219 <mhamza219@gmail.com>
2023-05-18 15:49:49 +0530

    card format in apidata

--- src/components/ApiData.js
+++ src/components/ApiData.js
@@ -1,55 +1,18 @@
-/*
-import React, {useEffect} from 'react'
-import axios from 'axios'
-
-
-function FetchData() {
-	useEffect(()=>{
-		axios.get(https://jsonplaceholder.typicode.com/users)
-			.then(res => console.log(res))
-			.catch(err => console.log(err));
-	},[])
-
-	return (
-		<div>FetchData</div>
-	)
-
-}
-
-*/
-
-
-/*
-import axios from "axios";
-import React from "react";
-
-//const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
-const baseURL = "https://jsonplaceholder.typicode.com/users/1";
-
-export default function ApiData() {
-  const [post, setPost] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  if (!post) return null;
-
-  return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
-  );
-}
-
-*/
 
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
 
 const ApiData = () => {
   const [data, setData] = useState([]);
@@ -68,14 +31,34 @@ const ApiData = () => {
   };
 
   return (
+  	<>
     <div>
       <h1>Data from API</h1>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
     </div>
+    <div>
+     {data.map((item) => (
+      <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {item.name[0][0]}
+          </Avatar>
+        }
+      
+        title= {item.name}
+        subheader={item.email}/>
+      
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {item.address.street }`
+          {item.address.city }
+        </Typography>
+      </CardContent>
+      
+    </Card>
+        ))}
+    </div>
+    </>
   );
 };
 
